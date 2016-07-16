2ab964928f68f44f4d8618a6cc43b09ffd1392e8
Rikard Fröberg <rikard@morus.se>
2016-07-16 17:57:42 +0200

    solutions to loop challenge exercise

---
+++ org/progund/lists/LoopChallengeVersion2.java
@@ -0,0 +1,32 @@
+package org.progund.lists;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LoopChallengeVersion2{
+
+  private final static int NO_ARGUMENTS=1;
+  public static void main(String[] args){
+    if(args.length == 0){
+      System.err.println("You must provide at least one argument.");
+      System.exit(NO_ARGUMENTS);
+    }
+
+    List<String> argList = Arrays.asList(args);
+
+    System.out.println("You gave " + args.length + " argument(s)");
+
+    for(String arg : argList){
+      System.out.println(arg);
+    }
+
+    System.out.println("Here they are in reverse order:");
+    
+    Collections.reverse(argList);
+
+    for(String arg : argList){
+      System.out.println(arg);
+    }
+  }
+}

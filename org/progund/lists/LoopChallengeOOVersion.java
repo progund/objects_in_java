aa7a7b42ac993f7b3e20a4e6e201ff44e66cfcc8
Rikard Fröberg <rikard@morus.se>
2016-07-16 18:49:51 +0200

    Added an OO version of the solution to the challenge

---
+++ org/progund/lists/LoopChallengeOOVersion.java
@@ -0,0 +1,13 @@
+package org.progund.lists;
+
+public class LoopChallengeOOVersion{
+  public static void main(String[] args){
+    ArgumentHelper argHelper = new ArgumentHelper(args);
+    if(argHelper.hasArguments()){
+      argHelper.start();
+    }else{
+      System.err.println(argHelper.errorMessage());
+    }
+    System.exit(argHelper.exitCode());
+  }
+}

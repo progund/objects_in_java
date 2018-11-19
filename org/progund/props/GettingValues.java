e3818d018e46986f9c7f55c7bcc00848f2e84ad6
Rikard Fröberg <rikard@morus.se>
2018-11-19 21:40:19 +0100

    fixes code style

--- org/progund/props/GettingValues.java
+++ org/progund/props/GettingValues.java
@@ -2,13 +2,16 @@ package org.progund.props;
 
 import java.util.Properties;
 
-public class GettingValues{
-  public static void main(String[] args){
+public class GettingValues {
+  
+  public static void main(String[] args) {
+    
     Properties systemProps = System.getProperties();
     String javaVersion = systemProps.getProperty("java.version");
     String operatingSystem = systemProps.getProperty("os.name");
 
     System.out.print("I'm running Java " + javaVersion);
     System.out.println(" on a " + operatingSystem + " machine.");
+    
   }
 }
--- org/progund/props/ListSystemProps.java
+++ org/progund/props/ListSystemProps.java
@@ -2,9 +2,9 @@ package org.progund.props;
 
 import java.util.Properties;
 
-public class ListSystemProps{
+public class ListSystemProps {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Properties systemProps = System.getProperties();
     systemProps.list(System.out);
   }
